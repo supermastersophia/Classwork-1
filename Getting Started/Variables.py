ad3383bca9ac94526a00777585b82ee64c64c76e
Sophia Ai <sofiamasterx10@gmail.com>
2025-07-23 16:48:04 -0700

    Create Variables.py

---
+++ Getting Started/Variables.py
@@ -0,0 +1,40 @@
+# Variables are like labels
+
+#Ex 1
+fred = 100
+print(fred)
+#result = 100
+
+#Ex 2
+fred = 200
+print(fred)
+#result = 200
+
+# More labels
+fred = 200
+john = fred 
+print(john)
+#result = 200
+
+coins = 200
+print(coins)
+#result = 200
+
+# Using Variables
+20 + 10 * 365
+#result = 3670
+3 * 52
+#result = 156
+3760 - 156
+#result = 3514
+
+# 1 line code (same result)
+20 + 10 * 365 - 3 * 52
+#result = 3514
+
+found = 20
+magic = 10
+stolen = 3
+
+found + magic * 365 - stolen * 52
+#result = 3514
